85c1fea70b383606afe4bb117836ac6ee0e9add6
robertdavid1205 <robertdavid1205@gmail.com>
2022-02-27 16:34:43 +0000

    completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -27,6 +27,7 @@ def cache_checkout_data(request):
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
+
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -35,19 +36,23 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         form_data = {
-                'full_name': request.POST['full_name'],
-                'email': request.POST['email'],
-                'phone_number': request.POST['phone_number'],
-                'country': request.POST['country'],
-                'postcode': request.POST['postcode'],
-                'town_or_city': request.POST['town_or_city'],
-                'street_address1': request.POST['street_address1'],
-                'street_address2': request.POST['street_address2'],
-                'county': request.POST['county'],
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -100,7 +105,6 @@ def checkout(request):
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
-
 
     template = 'checkout/checkout.html'
     context = {
