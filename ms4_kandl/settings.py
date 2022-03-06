dfbe7072c7f843b3765925a96aacf0bc52292546
robertdavid1205 <robertdavid1205@gmail.com>
2022-03-06 17:10:00 +0000

    sorted error sending real emails

--- ms4_kandl/settings.py
+++ ms4_kandl/settings.py
@@ -183,6 +183,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
+    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kandl-ms4'
