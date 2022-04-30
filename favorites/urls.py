0c0479915ea63a005c72d8018bb9b6a84298b5bd
robertdavid1205 <robertdavid1205@gmail.com>
2022-04-30 14:43:54 +0000

    deploy to heroku via CLI

--- favorites/urls.py
+++ favorites/urls.py
@@ -7,6 +7,10 @@ from . import views
 
 urlpatterns = [
     path('', views.view_product_favorites, name='view_product_favorites'),
-    path('add_product_to_favorites/<item_id>/', views.add_product_to_favorites, name='add_product_to_favorites'),
-    path('remove_product_from_favorites/<item_id>/<redirect_from>/', views.remove_product_from_favorites, name='remove_product_from_favorites'),
+    path('add_product_to_favorites/<item_id>/',
+         views.add_product_to_favorites,
+         name='add_product_to_favorites'),
+    path('remove_product_from_favorites/<item_id>/<redirect_from>/',
+         views.remove_product_from_favorites,
+         name='remove_product_from_favorites'),
 ]
