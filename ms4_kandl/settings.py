ff6be3fce2717508ea29674128463f75515a84dc
robertdavid1205 <robertdavid1205@gmail.com>
2022-05-27 17:44:02 +0000

    Update environment veriables for settings

--- ms4_kandl/settings.py
+++ ms4_kandl/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False')
 
 ALLOWED_HOSTS = ['kandl-ms4.herokuapp.com', 'localhost']
 
